9d84622da0473d092481c1dad8acaa786b0e1634
ManisaBasnet <manishabasnet601@gmail.com>
2024-09-30 11:48:44 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require("./model/index")
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs')
+
+
+
+app.get('/',(req ,res)=>{
+    res.render("home.ejs")
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about.ejs")
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("Project has been started");
+})
